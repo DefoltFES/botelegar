91ed8d55fb013b89666723047fea2b2b514d0627
DefoltFES <61965128+DefoltFES@users.noreply.github.com>
2021-01-06 16:41:30 +0300

    Initial Commit

---
+++ TelegrammAspMvcDotNetCoreBot/Models/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelegrammAspMvcDotNetCoreBot.Models
+{
+    public static class AppSettings
+    {
+        public static string Url { get; set; } = "https://botelegar.herokuapp.com:443/{0}";
+        public static string Name { get; set; } = "BadInsultbot";
+        public static string Key { get; set; } = "1553473590:AAFGSAxdg5Ig4BPExD9FQS_d-4DXvZ3BEsE";
+    }
+}
---
+++ TelegrammAspMvcDotNetCoreBot/Models/Commands/GenerateCommand.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegrammAspMvcDotNetCoreBot.Models.Commands
+{
+    public class GenerateCommand : Command
+    {
+        public override string Name => @"/generate";
+
+        public override bool Contains(Message message)
+        {
+            if (message.Type != Telegram.Bot.Types.Enums.MessageType.TextMessage)
+                return false;
+
+            return message.Text.Contains(this.Name);
+        }
+
+        public override async Task Execute(Message message, TelegramBotClient client)
+        {
+            var chatId = message.Chat.Id;
+            string url = "https://evilinsult.com/generate_insult.php?lang=en&type=json";
+
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+            string response;
+
+            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+            {
+                response = streamReader.ReadToEnd();
+            }
+
+            InsultInfo insultInfo = JsonConvert.DeserializeObject<InsultInfo>(response);
+            await client.SendTextMessageAsync(chatId, insultInfo.Insult, parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown);
+        }
+    }
+}
